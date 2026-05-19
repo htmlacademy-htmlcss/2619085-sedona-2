ce6001d914dadec2cf1401b597a76d95905a3a29
Darya Chirkova <d.chirkova11@gmail.com>
2026-05-19 14:42:04 +0300

    Финальная версия

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-const openModalButton = document.querySelector('.search-button');
+const openModalButton = document.querySelector('.search-btn');
 const modal = document.querySelector('.modal');
 const closeModalButton = document.querySelector('.close-modal');
 
 
 openModalButton.addEventListener('click', () => {
-    modal.classList.add('modal-show');
+  modal.classList.add('modal-show');
 });
 
 closeModalButton.addEventListener('click', () => {
-    modal.classList.remove('modal-show');
+  modal.classList.remove('modal-show');
 });
